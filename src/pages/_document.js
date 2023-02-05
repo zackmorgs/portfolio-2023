f81cef859f80bda954f279454a8afb2ac27e1680
zackmorgs <zackmorgenthaler@gmail.com>
2023-02-05 10:01:53 -0600

    initial commit, created to find bug

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,52 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
+import React, { Component, StrictMode } from 'react'
+
+function MainNav() {
+  const [navClosed, setNav] = React.useState(true);
+  function handleBurgerClick() {
+    setNav(!navClosed);
+    console.log("test")
+  }
+  return (
+    <nav id="nav-main">
+      <div id="nav-brand">
+        <Link href="/">
+          Z
+        </Link>
+        <button id="navbar_burger" onClick={handleBurgerClick} className={navClosed ? 'is-closed' : 'is-open'}>
+          <span></span> 
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+      <ul>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/music">Music</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+
+}
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <StrictMode>
+      <Html lang="en">
+        <Head />
+        <body>
+          <MainNav/>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </StrictMode>
   )
 }
