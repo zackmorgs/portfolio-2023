23334b2860a4aea8366a79e0d17290e69bfe66c7
zackmorgs <zackmorgenthaler@gmail.com>
2023-02-12 09:54:10 -0600

    Messed with background and favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,12 @@ export default function Document() {
           <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         <body>
+          <div className="bg">
+            <div className="wave" id="wave-1"></div>
+            <div className="wave" id="wave-2"></div>
+            <div className="wave" id="wave-3"></div>
+            <div className="wave" id="wave-4"></div>
+          </div>
           <Main />
           <NextScript />
         </body>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,13 +10,15 @@ export default function Home() {
         <title>Zack Morgenthaler</title>
         <meta name="description" content="A personal website " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+
         <img className={styles.profile} src="/img/playing-guitar.jpg"></img>
         <h1>Zack Morgenthaler</h1>
         <p className={styles.subheading}>Programmer, Music Maker, and Artist</p>
-        
+        <Link href="/about" className="btn">
+          About Me
+        </Link>
       </main>
     </>
   )
